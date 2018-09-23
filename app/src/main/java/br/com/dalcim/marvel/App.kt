5c10436984fefe3c52f6c2518f06f37040bf1102
trancosowilliam <wileleh@gmail.com>
2018-09-23 11:57:43 -0300

    add application class and startKoin with retrofitClientModule

---
+++ app/src/main/java/br/com/dalcim/marvel/App.kt
@@ -0,0 +1,18 @@
+package br.com.dalcim.marvel
+
+import android.app.Application
+import br.com.dalcim.marvel.module.applicationModule
+import br.com.dalcim.marvel.module.retrofitClientModule
+import org.koin.android.ext.android.startKoin
+
+class App : Application() {
+
+    private val modules = listOf (
+            retrofitClientModule
+    )
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, modules)
+    }
+}
