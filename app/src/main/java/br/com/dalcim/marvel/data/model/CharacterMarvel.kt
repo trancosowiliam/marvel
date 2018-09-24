b94e9d2b8aac0d6c6b93e61e34423cae83e9054b
trancosowilliam <wileleh@gmail.com>
2018-09-24 01:01:16 -0300

    More info in Character Marvel

--- app/src/main/java/br/com/dalcim/marvel/data/model/CharacterMarvel.kt
+++ app/src/main/java/br/com/dalcim/marvel/data/model/CharacterMarvel.kt
@@ -5,10 +5,18 @@ import br.com.dalcim.marvel.data.repository.remote.dto.CharacterMarvelDto
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class CharacterMarvel(val id: Long, val name: String, val image:String) : Parcelable {
+data class CharacterMarvel(
+        val id: Long,
+        val name: String,
+        val description: String,
+        val image: String,
+        val comicsAvailable: Int
+) : Parcelable {
     constructor(dto: CharacterMarvelDto) : this(
             id = dto.id,
             name = dto.name,
-            image = dto.image
+            description = dto.description,
+            image = dto.image,
+            comicsAvailable = dto.comics.available
     )
 }
